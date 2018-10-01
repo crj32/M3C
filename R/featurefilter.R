45c0367181b2b83de74858cdb80bb3df5ced7103
crj32 <chris.r.john86@gmail.com>
2018-10-01 07:19:24 +0100

    removed percentage error

--- R/featurefilter.R
+++ R/featurefilter.R
@@ -1,23 +1,23 @@
-#' featurefilter: A function for filtering genes based on the coefficient of variance (CV)
+#' featurefilter: A function for filtering features based on the coefficient of variance (CV)
 #'
 #' @param mydata Data frame or matrix or M3C results object: if dataframe/matrix should have samples as columns and rows as features
-#' @param mypercentile Numerical value: CV percentile by which to filter the features, e.g. 90 indicates top 10% based on CV
+#' @param percentile Numerical value: CV percentile by which to filter the features, e.g. 10 indicates top 10 percent based on CV
 #'
 #' @return A filtered (fewer rows) data frame
 #' @export
 #'
 #' @examples
-#' filtered <- featurefilter(mydata,mypercentile=90)
+#' filtered <- featurefilter(mydata,percentile=10)
 
-featurefilter <- function(mydata,mypercentile=75){
-  message(paste('extracting the most variable: '),mypercentile,'% of features')
+featurefilter <- function(mydata,percentile=10){
+  message(paste('extracting the most variable: '),percentile,'percent of features')
   message(paste('features to start with:',nrow(mydata)))
-  # mypercentile (convert to decimal below)
-  mypercentile <- mypercentile/100
+  # percentile (convert to decimal below)
+  percentile <- 1-(percentile/100)
   # calc co efficient of variation for all rows (features)
   CV <- apply(mydata,1,sd)/(rowMeans(mydata))
   # get features with CV in the given percentile
-  CVthresh <- quantile(CV, mypercentile) 
+  CVthresh <- quantile(CV, percentile) 
   # filter data
   names <- names(CV)[CV>=as.numeric(CVthresh)]
   filtered_data <- subset(mydata, row.names(mydata) %in% names)
